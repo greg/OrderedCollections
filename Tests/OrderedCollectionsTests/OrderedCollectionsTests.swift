2eadd70c38d2180502d2d38315e2a69c5e11ca7f
Greg Omelaenko <greg@omelaen.co>
2020-07-23 02:50:05 +0800

    uncomment the OrderedDictionary test and fix its name

--- Tests/OrderedCollectionsTests/OrderedCollectionsTests.swift
+++ Tests/OrderedCollectionsTests/OrderedCollectionsTests.swift
@@ -2,18 +2,18 @@ import XCTest
 @testable import OrderedCollections
 import TestValues
 
-final class OrderedCollectionsTestsTests: XCTestCase {
+final class OrderedCollectionsTests: XCTestCase {
     
     func testOrderedDictionary() {
         var a: OrderedDictionary = [5: "hello", 6: "aoeu", -1: ""]
-//        XCTAssertEqual(a[5], "hello")
-//        XCTAssertEqual(a[6], "aoeu")
-//        XCTAssertEqual(a[-1], "")
-//        a[2] = "htns"
-//        XCTAssertEqual(a[2], "htns")
-//        a[5] = "bye"
-//        XCTAssertEqual(a[5], "bye")
-//        a.removeValueForKey(6)
-//        XCTAssertEqual(a.count, 3)
+        XCTAssertEqual(a[5], "hello")
+        XCTAssertEqual(a[6], "aoeu")
+        XCTAssertEqual(a[-1], "")
+        a[2] = "htns"
+        XCTAssertEqual(a[2], "htns")
+        a[5] = "bye"
+        XCTAssertEqual(a[5], "bye")
+        a[6] = nil
+        XCTAssertEqual(a.count, 3)
     }
 }
